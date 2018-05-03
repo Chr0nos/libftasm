29f030b439447b4c3c536cd9abd6547ecda6c435
Sébastien Nicolet <snicolet@student.42.fr>
2018-05-03 21:57:36 +0200

    added start for ft_memalloc

--- libfts.h
+++ libfts.h
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/30 18:49:44 by snicolet          #+#    #+#             */
-/*   Updated: 2018/05/03 20:55:47 by snicolet         ###   ########.fr       */
+/*   Updated: 2018/05/03 21:18:21 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ int				ft_strcmp(const char *s1, const char *s2);
 void			*ft_memalloc(size_t size);
 char			*ft_strrchr(char *str, int c);
 void			ft_memdel(void **ptr);
+char			*ft_strnew(size_t size);
 
 #endif
--- srcs/ft_memalloc.s
+++ srcs/ft_memalloc.s
@@ -3,13 +3,26 @@ section .text align=16
 	extern _malloc
 	extern _ft_bzero
 
+;                    rdi (r8)
 ;void *ft_memalloc(size_t size);
-_fT_memalloc:
-	call _malloc
-	cmp rax, 0
+_ft_memalloc:
+	push rbp
+	mov rbp, rsp
+
+	; void *malloc(size_t)
+	call _malloc			; malloc with size
+	cmp rax, 0				; check for NULL ptr
 	je .quit
-	mov rdi, rax
-	mov rsi, rdi
+	mov r8, rax				; r8 = ptr
+	mov r15, rdi			; r15 = size
+
+	;			rdi			rsi
+	; bzero(void *ptr, size_t size);
+	mov rdi, r8				; ptr
+	mov rsi, r15			; size
 	call _ft_bzero
+
+;	mov rax, r8		; return ptr
 	.quit:
+		pop rbp
 		ret
