b87691b1e80e3c1dec103d3347393ff14c69c3bd
Sebastien NICOLET <snicolet@student.42.fr>
2018-05-03 18:50:19 +0200

    fixed ft_strcmp

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -7,21 +7,21 @@ _ft_strcmp:
 	push rbp
 	mov rbp, rsp
 
-	xor r8, r8							; reset register
-	xor r9, r9							; reset register
 	mov r15, 0							; setting counter to 0
 	.loop:
+		xor r8, r8						; reset register
+		xor r9, r9						; reset register
 		mov r8b, byte[rdi + r15]		; storing char 1 in r8b
 		mov r9b, byte[rsi + r15]		; storing char 2 in r9b
-		cmp r8b, 0						; checking for end of line for char 1
+		cmp r8d, 0						; checking for end of line for char 1
 		je .quit						; quit if so
 		inc r15							; otherwise incrase the counter
-		cmp r8b, r9b					; check of char 1 == char 2
+		cmp r8d, r9d					; check of char 1 == char 2
 		je .loop						; loop is equal, otherwise quit
 
 	.quit:
 		xor rax, rax					; reset rax
-		mov al, r9b						; setting char 2 to value
-		sub al, r8b						; substractif char 1 value
+		mov eax, r8d						; setting char 2 to value
+		sub eax, r9d						; substractif char 1 value
 		pop rbp
 		ret
