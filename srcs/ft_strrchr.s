757b8e25f291206630e88dc3b5947686b71bb8c5
Sébastien Nicolet <snicolet@student.42.fr>
2018-05-04 17:57:15 +0200

    center

--- srcs/ft_strrchr.s
+++ srcs/ft_strrchr.s
@@ -2,26 +2,23 @@ section .text align=16
 	global _ft_strrchr
 	extern	_ft_strlen
 
+;					rdi			rsi
 ;char	*ft_strrchr(char *src, int c);
 _ft_strrchr:
-	mov r8b, dil						; backup original src in r8
+	mov r8, rdi							; backup original src in r8
 	call _ft_strlen
 	cmp rax, 0
 	je .quit
 	add rdi, rax						; set the src ptr to the end
 
-	; check if c == '\0', if so i return the current position and go
-	cmp sil, 0
-	jne .loop
-	mov rax, rdi
-	jmp .quit
-
 	.loop:
 		cmp BYTE [rdi], sil				; read char and cmp with c
 		je .found
 		dec rdi							; go to previous char
-		cmp BYTE [rdi], r8b				; check if ptr < src
+		cmp rdi, r8						; check if ptr < src
 		jge .loop						; if not then next loop iteration
+		mov rax, 0						; return NULL (not found)
+		jmp .quit
 
 	.found:
 		mov rax, rdi
